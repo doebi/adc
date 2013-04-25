2dd9dc5b168ea346f1b15be2b31eec9177c51d01
doebi <doebi3d@gmail.com>
2013-04-25 15:53:18 +0200

    first commit. working template engine.

---
+++ adc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#tool for automatic generation of documentation
+
+from Cheetah.Template import Template
+import time
+from datetime import date
+
+#open file for writing
+f = open('compiled.tex', 'w')
+
+#defining data for use in template
+nameSpace = {
+             'author': 'Max Mustermann',
+             'course': 'ADF2 / PRO2 SS 13',
+             'date': date.today().isoformat()
+            }
+
+#define template
+t = Template(file="templates/default.tex", searchList=[nameSpace])
+
+#writing template to file
+f.write(str(t))
+f.close
